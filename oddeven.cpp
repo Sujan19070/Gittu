8e659a004cec1470953793b62d34cb5e43aef02c
Nabib Ahamed <sujanofficial19070@gmail.com>
2026-08-19 08:40:52 +0600

    testing gitignore final

---
+++ oddeven.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    
+    // Get input from user
+    cout << "Enter a number: ";
+    cin >> number;
+    
+    // Check if number is even or odd
+    if (number % 2 == 0) {
+        cout << number << " is an even number." << endl;
+    } else {
+        cout << number << " is an odd number." << endl;
+    }
+    
+    return 0;
+}
